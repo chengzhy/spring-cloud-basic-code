84ee6b8b9dc1b5a3e35dd8676437695a8acdd016
chengzhy <chengzhiyuan@inspur.com>
2021-09-08 10:36:29 +0800

    修改

---
+++ src/test/java/com/github/chengzhy/basiccode/SpringCloudBasicCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.chengzhy.basiccode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudBasicCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
